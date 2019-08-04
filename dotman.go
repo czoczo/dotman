5cfb2188d004283e4940b786fd90180f0e1419b7
czoczo <mr.czoczo@gmail.com>
2019-08-04 02:42:37 +0200

    added git clone and pull support. serving files over http. downloading bash script drafts.

--- dotman.go
+++ dotman.go
@@ -1,23 +1,136 @@
 package main
 
-import s "strings"
 import (
 	"fmt"
 	"net/http"
     "log"
     "regexp"
+	"os"
+    "strings"
+    "io/ioutil"
+    "path/filepath"
+    "golang.org/x/crypto/ssh"
+    gitssh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+	git "gopkg.in/src-d/go-git.v4"
+	. "gopkg.in/src-d/go-git.v4/_examples"
 )
 
 func main() {
+	url := "ssh://git@cz0.cz:2222/czoczo/dotrepo.git"
+    directory := "dotfiles"
+    baseurl := "127.0.0.1:1337"
+
+    s := fmt.Sprintf("%s/.ssh/id_rsa", os.Getenv("HOME"))
+    sshKey, _ := ioutil.ReadFile(s)
+    signer, _ := ssh.ParsePrivateKey([]byte(sshKey))
+    auth := &gitssh.PublicKeys{User: "git", Signer: signer}
+
+    if _, err := os.Stat(directory); os.IsNotExist(err) {
+        // Clone the given repository to the given directory
+        Info("git clone %s %s", url, directory)
+
+        //	r, err := git.PlainClone(directory, false, &git.CloneOptions{
+        //		Auth: &http.BasicAuth{
+        //			Username: username,
+        //			Password: password,
+        //		},
+        //		URL:      url,
+        //		Progress: os.Stdout,
+        //	})
+
+
+        r, err := git.PlainClone(directory, false, &git.CloneOptions{
+            Auth: auth,
+            URL:      url,
+            Progress: os.Stdout,
+        })
+
+        CheckIfError(err)
+
+        // ... retrieving the branch being pointed by HEAD
+        ref, err := r.Head()
+        CheckIfError(err)
+        // ... retrieving the commit object
+        commit, err := r.CommitObject(ref.Hash())
+        CheckIfError(err)
+
+        fmt.Println(commit)
+
+    } else {
+        // We instance\iate a new repository targeting the given path (the .git folder)
+        r, err := git.PlainOpen(directory)
+        CheckIfError(err)
+
+        // Get the working directory for the repository
+        w, err := r.Worktree()
+        CheckIfError(err)
+
+        // Pull the latest changes from the origin remote and merge into the current branch
+        Info("git pull origin")
+        err = w.Pull(&git.PullOptions{
+            Auth: auth,
+            RemoteName: "origin",
+        })
+    //    CheckIfError(err)
+
+        // ... retrieving the branch being pointed by HEAD
+        ref, err := r.Head()
+        CheckIfError(err)
+        // ... retrieving the commit object
+        commit, err := r.CommitObject(ref.Hash())
+        CheckIfError(err)
+
+        fmt.Println(commit)
+    }
+
+    // serve static
+    fs := http.FileServer(http.Dir(directory))
+    http.Handle("/"+directory+"/", http.StripPrefix("/"+directory+"/", fs))
+
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		log.Println( r.URL.Path)
         list, _ := regexp.Compile("^/([0-9]+,?)+$")
         if r.URL.Path == "/" {
-		    fmt.Fprintf(w, "Lista")
+
+
+            files, err := ioutil.ReadDir(directory)
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            for _, f := range files {
+
+                    match, _ := regexp.MatchString("(.git|README.md)", f.Name())
+                    if match {
+                        continue
+                    }
+
+                    err = filepath.Walk(directory+"/"+f.Name(),
+                        func(path string, info os.FileInfo, err error) error {
+                        if err != nil {
+                            return err
+                        }
+
+                        if info.IsDir() {
+                            return nil
+                        }
+                        output := strings.TrimPrefix(path, directory+"/"+f.Name()+"/")
+                        if filepath.Dir(output) != "." {
+                            fmt.Fprint(w,"mkdir -p $HOME/" + filepath.Dir(output)+"\n")
+                        }
+
+
+                        fmt.Fprint(w,"curl " + "http://" + baseurl + "/" + path + " > $HOME/" + output+"\n")
+                        return nil
+                    })
+                    if err != nil {
+                        log.Println(err)
+                    }
+            }
             return
         }
         if list.MatchString(r.URL.Path) {
-            slice := s.Split(s.Replace(r.URL.Path,"/","",-1), ",")
+            slice := strings.Split(strings.Replace(r.URL.Path,"/","",-1), ",")
             var response string
             for _, num := range slice {
                 response = response +" | "+ num
