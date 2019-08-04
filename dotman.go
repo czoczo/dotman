de43d8213a60ba06299be2f8e5cb18613de92a4c
czoczo <mr.czoczo@gmail.com>
2019-08-05 01:32:57 +0200

    added mapping folders to char's. Updated choosing URL method

--- dotman.go
+++ dotman.go
@@ -19,6 +19,7 @@ func main() {
 	url := "ssh://git@cz0.cz:2222/czoczo/dotrepo.git"
     directory := "dotfiles"
     baseurl := "127.0.0.1:1337"
+    alphabet := "01234567890abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ"
 
     s := fmt.Sprintf("%s/.ssh/id_rsa", os.Getenv("HOME"))
     sshKey, _ := ioutil.ReadFile(s)
@@ -89,7 +90,8 @@ func main() {
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		log.Println( r.URL.Path)
-        list, _ := regexp.Compile("^/([0-9]+,?)+$")
+        //commaListRegex, _ := regexp.Compile("^/([0-9a-zA-Z]+,?)+$")
+        listRegex, _ := regexp.Compile("^/[0-9a-zA-Z]+$")
         if r.URL.Path == "/" {
 
 
@@ -98,12 +100,21 @@ func main() {
                 log.Fatal(err)
             }
 
+            if len(files) > len(alphabet) {
+                    fmt.Fprint(w,"Congratz, you reached the limit of number of supported folders. Reach me or implement it yourself (or both ;) )")
+            }
+
+            charCounter := 0
+
             for _, f := range files {
 
                     match, _ := regexp.MatchString("(.git|README.md)", f.Name())
                     if match {
                         continue
                     }
+
+                    fmt.Fprint(w,"\n"+string(alphabet[charCounter])+") "+f.Name()+" \n")
+                    charCounter = charCounter+1
 
                     err = filepath.Walk(directory+"/"+f.Name(),
                         func(path string, info os.FileInfo, err error) error {
@@ -129,11 +140,20 @@ func main() {
             }
             return
         }
-        if list.MatchString(r.URL.Path) {
-            slice := strings.Split(strings.Replace(r.URL.Path,"/","",-1), ",")
+//        if commaListRegex.MatchString(r.URL.Path) {
+//            slice := strings.Split(strings.Replace(r.URL.Path,"/","",-1), ",")
+//            var response string
+//            for _, num := range slice {
+//                response = response +" | "+ num
+//            }
+//		    fmt.Fprintf(w, response)
+//            return
+//        }
+        if listRegex.MatchString(r.URL.Path) {
+            choice := strings.Replace(r.URL.Path,"/","",-1)
             var response string
-            for _, num := range slice {
-                response = response +" | "+ num
+            for _, char :=  range choice {
+                response = response +" | "+ string(char)
             }
 		    fmt.Fprintf(w, response)
             return
